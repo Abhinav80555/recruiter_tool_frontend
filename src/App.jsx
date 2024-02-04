8ba547d6a5fc64d8a1f8836844340d2efe058de4
abhinav <abhinavs@khojguru.com>
2024-02-04 21:48:21 +0530

    get all user data integration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material";
 import { Delete, Edit, MoreVert } from "@mui/icons-material";
 import { AddUser } from "./components/AddUser";
+import { baseURL } from "./API/helpher";
 
 function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john@example.com",
-      phone: "9876543210",
-      status: "Contacted",
-      expectedSalary: "600000",
-      computedScore: "3",
-      skills: "react,node,mongodb,angular,mysql,threejs,css,html,js",
-      qualification: "b.e",
-    },
-    {
-      id: 2,
-      name: "Jane Doe",
-      email: "jane@example.com",
-      phone: "9876543210",
-      status: "Interview Scheduled",
-      expectedSalary: "600000",
-      computedScore: "5",
-      skills: "react,node,mongodb",
-      qualification: "master of bussiness administration",
-    },
-    {
-      id: 3,
-      name: "Alice Smith",
-      email: "alice@example.com",
-      phone: "9876543210",
-      status: "Offer Extended",
-      expectedSalary: "600000",
-      computedScore: "7",
-      skills: "react,node,mongodb,angular,mysql,threejs,css,html,js",
-      qualification: "b.e",
-    },
-    {
-      id: 4,
-      name: "Bob Johnson",
-      email: "bob@example.com",
-      phone: "9876543210",
-      status: "Hired",
-      expectedSalary: "600000",
-      computedScore: "2",
-      skills: "react,node,mongodb",
-      qualification: "b.e",
-    },
-    {
-      id: 5,
-      name: "Abhinav",
-      email: "bob@example.com",
-      phone: "9876543210",
-      status: "Rejected",
-      expectedSalary: "600000",
-      computedScore: "1",
-      skills: "react,node,mongodb,angular,mysql,threejs,css,html,js",
-      qualification: "bachelor of engineering",
-    },
-    // Add more sample data as needed
-  ]);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [usersPerPage] = useState(5);
-  const [searchTerm, setSearchTerm] = useState("");
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const [limit, setLimit] = useState(10);
+  const [search, setSearch] = useState();
+  const [page, setPage] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [loading, setLoading] = useState(false);
+  // --------------------------------
+  const [finalData, setFinalData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${baseURL}user/getall?page=${page + 1}&limit=${limit}&search=${search}`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (data) {
+        setFinalData(data);
+
+        let newData = [];
+
+        setTotalItems(data && data.total_items);
+
+        data &&
+          data.data &&
+          data.data.map((item, index) => {
+            newData.push({
+              slno: page * limit + index + 1,
+              ...item,
+            });
+          });
+        if (newData && newData.length > 0) {
+          setFinalData(newData);
+        } else {
+          setFinalData([]);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [limit, page, search]);
 
   const openDialog = () => {
     console.log("kasas");
@@ -88,17 +74,16 @@ function App() {
     setAnchorEl(null);
   };
 
-  // Pagination
-  const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = indexOfLastUser - usersPerPage;
-  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+  const indexOfLastUser = (page + 1) * limit;
+  const indexOfFirstUser = indexOfLastUser - limit;
+  const currentUsers = finalData.slice(indexOfFirstUser, indexOfLastUser);
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
+  const handleSearch = (event) => {};
 
   // Pagination change handler
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const handlePagination = (pageNumber) => {
+    setPage(pageNumber);
+  };
 
   return (
     <div className="container mx-auto">
@@ -107,7 +92,6 @@ function App() {
         <input
           type="text"
           placeholder="Search by name"
-          value={searchTerm}
           onChange={handleSearch}
           className="px-2 py-0 mr-2 border border-gray-300 rounded"
         />
@@ -135,15 +119,15 @@ function App() {
           </tr>
         </thead>
         <tbody className="bg-white max-h-[50vh] overflow-y-auto overflow-x-hidden">
-          {currentUsers.map((user) => (
+          {finalData.map((user) => (
             <tr key={user.id} className="hover:bg-gray-100">
-              <td className="px-6 py-4">{user.id}</td>
+              <td className="px-6 py-4">{user.slno}</td>
               <td className="px-6 py-4">{user.name}</td>
               <td className="px-6 py-4">{user.email}</td>
               <td className="px-6 py-4">{user.phone}</td>
               <td className="px-6 py-4">{user.status}</td>
-              <td className="px-6 py-4">{user.expectedSalary}</td>
-              <td className="px-6 py-4">{user.computedScore}</td>
+              <td className="px-6 py-4">{user.expected}</td>
+              <td className="px-6 py-4">{user.score}</td>
               <td className="px-6 py-4">
                 <IconButton
                   id="basic-button"
@@ -207,56 +191,39 @@ function App() {
         </tbody>
       </table>
       {/* Pagination */}
-      {/* <div className="mt-4 px-5">
-        <ul className="flex justify-end item-center list-none">
-          {[...Array(Math.ceil(users.length / usersPerPage)).keys()].map(
-            (number) => (
-              <li key={number} className="mx-1">
-                <button
-                  onClick={() => paginate(number + 1)}
-                  className={`${
-                    currentPage === number + 1
-                      ? "bg-blue-500 text-white"
-                      : "bg-gray-200"
-                  } px-3 py-1 rounded`}
-                >
-                  {number + 1}
-                </button>
-              </li>
-            )
-          )}
-        </ul>
-      </div> */}
       <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 mt-4 sm:px-6">
-  <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-    <div>
-      <p className="text-sm text-gray-700">
-        Showing <span className="font-medium">{indexOfFirstUser + 1}</span> to{' '}
-        <span className="font-medium">{Math.min(indexOfLastUser, users.length)}</span> of{' '}
-        <span className="font-medium">{users.length}</span> results
-      </p>
-    </div>
-    <div>
-      <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-        {[...Array(Math.ceil(users.length / usersPerPage)).keys()].map((number) => (
-          <a
-            key={number}
-            href="#"
-            onClick={() => paginate(number + 1)}
-            className={`${
-              currentPage === number + 1
-                ? 'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                : 'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'
-            }`}
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{page * limit + 1}</span> to{" "}
+            <span className="font-medium">
+              {Math.min(page + 1 * limit, totalItems)}
+            </span>{" "}
+            of <span className="font-medium">{totalItems}</span> results
+          </p>
+        </div>
+        <div>
+          <nav
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
           >
-            {number + 1}
-          </a>
-        ))}
-      </nav>
-    </div>
-  </div>
-</div>
-
+            {Array.from({ length: Math.ceil(totalItems / limit) }).map(
+              (_, index) => (
+                <button
+                  key={index}
+                  onClick={() => handlePagination(index)}
+                  className={`${
+                    page === index
+                      ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              )
+            )}
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
